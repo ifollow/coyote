d318c572eea082db2ed04b1373933d1192f7740c
Antonio Ricardo <renatoauto@ifollowagencia.com.br>
2014-04-14 09:46:21 -0400

    Primeiro commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,34 @@
+class HomeController < ApplicationController
+
+  def index
+  	
+  end
+
+  def casa
+  	
+  end
+
+  def agenda
+  	
+  end
+
+  def cardapio
+  	
+  end
+
+  def ingressos
+  	
+  end
+
+  def midia
+  	
+  end
+
+  def novidades
+  	
+  end
+
+  def contato
+  	
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coyote::Application.config.secret_key_base = '1fbeae4c488b74de8fdd9841dd1f21f3331efc867bae5b38ebc3ac4fda1fab50f5936601b2beeeb24ce078938790fd1bf1a2f87265279cadaf4be2e9a3ec5a87'
